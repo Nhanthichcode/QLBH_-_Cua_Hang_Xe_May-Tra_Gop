f83e8689a3edb011a8152d18bef8d49fbc4e3c21
Nhanthichcode <nhanlx151@gmail.com>
2026-04-09 15:15:14 +0700

    tệp khởi chạy chương trình

---
+++ help/F_Help.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace DoAnBanHang_cuahangxemay.help
+{
+    partial class F_Help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Help));
+            richTextBoxHelp = new RichTextBox();
+            SuspendLayout();
+            // 
+            // richTextBoxHelp
+            // 
+            richTextBoxHelp.BackColor = Color.MintCream;
+            richTextBoxHelp.BorderStyle = BorderStyle.None;
+            richTextBoxHelp.Font = new Font("Segoe UI", 10F);
+            richTextBoxHelp.Location = new Point(28, 12);
+            richTextBoxHelp.Name = "richTextBoxHelp";
+            richTextBoxHelp.ReadOnly = true;
+            richTextBoxHelp.Size = new Size(1112, 618);
+            richTextBoxHelp.TabIndex = 2;
+            richTextBoxHelp.Text = "";
+            // 
+            // F_Help
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MintCream;
+            ClientSize = new Size(1153, 640);
+            Controls.Add(richTextBoxHelp);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "F_Help";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Trợ Giúp";
+            Load += F_Help_Load;
+            KeyDown += F_Help_KeyDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private RichTextBox richTextBoxHelp;
+    }
+}
---
+++ help/F_Help.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnBanHang_cuahangxemay.help
+{
+    public partial class F_Help : Form
+    {
+        private string _context;
+
+        public F_Help(string context)
+        {
+            InitializeComponent();
+            _context = context;
+        }
+        private void LoadHelpContent()
+        {
+            string exeDir = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Lên 3 cấp từ /bin/Debug/netX.0/
+            string projectRoot = Path.GetFullPath(Path.Combine(exeDir, @"..\..\..\"));
+
+            // Ghép đường dẫn tới thư mục help
+            string helpFile = Path.Combine(projectRoot, "help", $"{_context}.txt");
+            try
+            {
+                if (File.Exists(helpFile))
+                {
+                    richTextBoxHelp.Text = File.ReadAllText(helpFile);
+                }
+                else
+                {
+                    richTextBoxHelp.Text = $"Không tìm thấy file trợ giúp tại:\n{helpFile}";
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message + " / " + ex.StackTrace); }
+        }
+
+        private void F_Help_Load(object sender, EventArgs e)
+        {
+            LoadHelpContent();
+        }
+
+        private void F_Help_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
